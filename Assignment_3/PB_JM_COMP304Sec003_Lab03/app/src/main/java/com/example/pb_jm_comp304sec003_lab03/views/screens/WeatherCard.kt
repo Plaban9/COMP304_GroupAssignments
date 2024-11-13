579c9f6ec0a1941d158da79c0f8256bd39a5a7a7
Plaban Biswas <plabanbiswas96@outlook.com>
2024-11-12 22:15:31 -0500

    - ADDED: Shadow and alignment in Async Image status icon

--- Assignment_3/PB_JM_COMP304Sec003_Lab03/app/src/main/java/com/example/pb_jm_comp304sec003_lab03/views/screens/WeatherCard.kt
+++ Assignment_3/PB_JM_COMP304Sec003_Lab03/app/src/main/java/com/example/pb_jm_comp304sec003_lab03/views/screens/WeatherCard.kt
@@ -2,6 +2,7 @@ package com.example.pb_jm_comp304sec003_lab03.views.screens
 
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -16,6 +17,7 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ElevatedCard
@@ -27,9 +29,15 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.BlurredEdgeTreatment
 import androidx.compose.ui.draw.blur
 import androidx.compose.ui.draw.scale
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Color.Companion.Black
+import androidx.compose.ui.graphics.Color.Companion.Green
+import androidx.compose.ui.graphics.FilterQuality
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.graphics.Shadow
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 
@@ -169,12 +177,19 @@ fun TemperatureEmoji(weatherData: WeatherData)
         ) {
 
             AsyncImage(
-                model = "https://" + weatherData.current.condition.icon,
-                contentDescription = "Weather Icon",
-                modifier = Modifier.size(60.dp),
-
+                    model = "https://" + weatherData.current.condition.icon,
+                    contentScale = ContentScale.Crop,
+                    clipToBounds = true,
+                    contentDescription = "Weather Icon",
+                    filterQuality = FilterQuality.High,
+                    modifier = Modifier
+                        .padding(0.dp)
+//                        .height(128.dp)
+//                        .width(128.dp)
+                        .scale(scaleX = 3.75f, scaleY = 3.75f)
+                        .offset(x = -2.dp, y = 2.dp)
+                        .shadow(3.dp, shape = RectangleShape, clip = true, ambientColor = Black, spotColor = Black)
             )
-
         }
     }
 }
