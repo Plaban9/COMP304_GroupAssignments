de923661205a1602dcf96db8ba865b384097aadc
EpicBlade <jaydeepmodha1@gmail.com>
2024-11-12 21:12:28 -0500

    Update WeatherCard.kt

--- Assignment_3/PB_JM_COMP304Sec003_Lab03/app/src/main/java/com/example/pb_jm_comp304sec003_lab03/views/screens/WeatherCard.kt
+++ Assignment_3/PB_JM_COMP304Sec003_Lab03/app/src/main/java/com/example/pb_jm_comp304sec003_lab03/views/screens/WeatherCard.kt
@@ -1,6 +1,7 @@
 package com.example.pb_jm_comp304sec003_lab03.views.screens
 
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -23,11 +24,21 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.BlurredEdgeTreatment
+import androidx.compose.ui.draw.blur
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shadow
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import coil3.compose.AsyncImage
+import com.example.pb_jm_comp304sec003_lab03.R
 
 import com.example.pb_jm_comp304sec003_lab03.models.WeatherData
 
