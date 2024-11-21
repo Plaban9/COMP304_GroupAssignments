dec7b62d843a0cb335a5e382a797f2c94bae8b9e
EpicBlade <jaydeepmodha1@gmail.com>
2024-11-21 15:54:36 -0500

    Update WeatherService.kt

--- Assignment_3/PB_JM_COMP304Sec003_Lab03/app/src/main/java/com/example/pb_jm_comp304sec003_lab03/services/WeatherService.kt
+++ Assignment_3/PB_JM_COMP304Sec003_Lab03/app/src/main/java/com/example/pb_jm_comp304sec003_lab03/services/WeatherService.kt
@@ -5,7 +5,7 @@ import com.example.pb_jm_comp304sec003_lab03.models.WeatherData
 import retrofit2.http.GET
 import retrofit2.http.Query
 
-private const val API_KEY = "f2c7c09f8c3d472b9d2181802241011"
+private const val API_KEY = "[YOUR WEATHER API KEY GOES HERE]"
 
 interface WeatherService
 {
